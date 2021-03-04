c956853e2568e0bf4f543c412a71bcfee672f97f
Sofia <sofimantas@gmail.com>
2021-03-04 14:46:37 -0500

    add app bar to main page, with login and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,52 @@
-import logo from './images/full-logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./mainlogo.svg";
+import "./App.css";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function App() { 
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
+         <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="light" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+          The Art Of Traveling Headache Free
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      </div>
+     
+     
     </div>
   );
 }
+
 
 export default App;
