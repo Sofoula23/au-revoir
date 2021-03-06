e8c83d0244a6d411ce57596a35122b43fc85146a
Sofia <sofimantas@gmail.com>
2021-03-06 14:54:40 -0500

    fix and clean up schema

--- models/Trip.js
+++ models/Trip.js
@@ -1,68 +1,87 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const tripSchema = new mongoose.Schema({
-    name: String, 
-    activities: [{
-      outdoors: String,
-      indoor: String,
-      ecoFriendly: String,
-      water: String,
-      cultural: String,
-      kidsFriendly: String,
-      extreme: String,
-      relaxed: String,
-      sightseeing: String
-    }],
-    restaurants: [{
-      name: String,
-      address: String, 
-      address2: String,
-      city: String,
-      province: String,
-      postal: String
-    }],
-    flights: [{
+  name: String,
+  flights: [
+    {
       flightNumber: String,
       airline: String,
       itinerary: String,
-      seats: [{
-        type: String
-      }]
-    }],
-    travelers: [{
+      seats: [
+        {
+          type: String,
+        },
+      ],
+    },
+  ],
+  travelers: [
+    {
       name: String,
       profilePicture: String,
-      age: Number
-    }],
-    destination: [{
-      continent: [{
-        europe: String,
-        southAmerica: String,
-        northAmerica: String,
-        africa: String,
-        asia: String,
-        australia: String,
-        antartica: String 
-      }],
+      age: Number,
+    },
+  ],
+  destinations: [
+    {
+      continent: {
+        type: String,
+        enum: [
+          "europe",
+          "southAmerica",
+          "northAmerica",
+          "africa",
+          "asia",
+          "australia",
+          "antartica",
+        ],
+      },
+
       country: String,
       province: String,
       city: String,
-    }],
-    stay: [{
-      hotel: String,
-      rental: String,
-      hostal: String,
-      couchsurf: String,
-      length: Number
-    }],
-    transportation: [{
-      type: String,
-      price: Number
-    }],    
-    other: [{
-      name: String
-    }] 
+      completed: Boolean,
+      stays: [
+        {
+          stayType: {
+            enum: ["hotel", "rental", "hostal", "couchsurf"],
+          },
+          startDate: Date,
+          endDate: Date,
+        },
+      ],
+      restaurants: [
+        {
+          name: String,
+          address: String,
+          address2: String,
+          city: String,
+          province: String,
+          postal: String,
+          completed: Boolean,
+        },
+      ],
+      activities: [
+        {
+          description: String,
+          completed: Boolean,
+          activityType: {
+            type: String,
+            enum: [
+              "outdoors",
+              "indoor",
+              "ecoFriendly",
+              "water",
+              "cultural",
+              "kidsFriendly",
+              "extreme",
+              "relaxed",
+              "sightseeing",
+            ],
+          },
+        },
+      ],
+    },
+  ],
+});
+const Trip = mongoose.model("Trip", tripSchema);
 
-  });
-  const Trip = mongoose.model('Trip', tripSchema);  
-
-module.exports = Trip; 
+module.exports = Trip;
