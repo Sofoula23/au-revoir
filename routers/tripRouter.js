c74b6adf096db87cb252cb9c3fc4ab23101b9641
Sofia <sofimantas@gmail.com>
2021-03-07 21:22:26 -0500

    add try catch to API endpoints t ensure express always sends a response

--- routers/tripRouter.js
+++ routers/tripRouter.js
@@ -4,16 +4,24 @@ const Trip = require("../models/Trip");
 
 // define the home page route
 router.get("/", async function (req, res, next) {
-  const trips = await Trip.find();
-  res.status(200).send(trips);
-  next();
+  try {
+    const trips = await Trip.find();
+    res.status(200).send(trips);
+    next();
+  } catch (e) {
+    next(e);
+  }
 });
 // define the about route
 router.post("/", async function (req, res, next) {
-  const newTrip = new Trip(req.body);
-  await newTrip.save();
-  res.status(201).send(newTrip);
-  next();
+  try {
+    const newTrip = new Trip(req.body);
+    await newTrip.save();
+    res.status(201).send(newTrip);
+    next();
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
