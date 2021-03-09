1ce93109040ff542c2584ef1599ae03d21a23dd6
Sofia <sofimantas@gmail.com>
2021-03-09 00:51:59 -0500

    add list of link cards to trips/home page

--- client/src/components/pages/Trips/index.js
+++ client/src/components/pages/Trips/index.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 import { useCurrentUser } from "../../../context/UserContext";
 import noTripsImage from "../../../images/undraw_void_3ggu.svg";
+import cevicheImage from "../../../images/ceviche.jpg";
 
 import "./styles.css";
 
@@ -39,6 +46,35 @@ function TripsPage() {
       </div>
     );
   }
+
+  return (
+    <div className="trips-page">
+      <Grid container spacing={2}>
+        {trips.map((trip) => (
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <Link to={`/trips/${trip._id}`} className="trip-link">
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={trip.destination.formatted_address}
+                    height="140"
+                    image={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${trip.destination.photos[0].photo_reference}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
+                    title={trip.destination.formatted_address}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {trip.destination.formatted_address}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Link>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
 
 export default TripsPage;
