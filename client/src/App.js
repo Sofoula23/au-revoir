4b5930084de055e91f543bf4a50cb7b8790b1e36
Sofia <sofimantas@gmail.com>
2021-03-10 23:36:56 -0500

    use browser router instead of hash router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { StylesProvider } from "@material-ui/core/styles";
 
 import UserContextProvider from "./context/UserContext";
